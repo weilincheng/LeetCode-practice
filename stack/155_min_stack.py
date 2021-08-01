900d0497f2b26fc1b1cea8cf53761a90400a647f
weinlincheng <7236196+weilincheng@users.noreply.github.com>
2021-08-01 15:04:50 +0800

    Updated 155

--- stack/155_min_stack.py
+++ stack/155_min_stack.py
@@ -2,27 +2,24 @@
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stack = []
         self.minStack = []
-        
+
 
     def push(self, val: int) -> None:
-        if len(self.minStack) == 0 or val <= self.minStack[len(self.minStack) - 1]:
-            self.minStack.append(val)
-        
+        newMin = val
+        if len(self.minStack):
+            newMin = min(self.minStack[len(self.minStack) - 1], val)
+            
+        self.minStack.append(newMin)
         self.stack.append(val)
         
 
     def pop(self) -> None:
-        if self.stack[len(self.stack) - 1] == self.minStack[len(self.minStack) - 1]:
-            self.minStack.pop()
-        
+        self.minStack.pop()
         self.stack.pop()
         
-
+        
     def top(self) -> int:
         return self.stack[len(self.stack) - 1]
         
