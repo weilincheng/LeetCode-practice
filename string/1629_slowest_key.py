498e564bda7e63c75f0384a9c0ef156c3758b270
weinlincheng <7236196+weilincheng@users.noreply.github.com>
2021-09-06 22:08:44 +0800

    Add 1629. Slowest Key

---
+++ string/1629_slowest_key.py
@@ -0,0 +1,14 @@
+class Solution:
+    # O(n) time | O(1) space - where n is the length of the input list
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        maxDuration, startTime, slowestKey = 0, 0, ''
+        for i in range(len(releaseTimes)):
+            pressTime = releaseTimes[i] - startTime
+            if pressTime > maxDuration:
+                maxDuration = pressTime
+                slowestKey = keysPressed[i]
+            elif pressTime == maxDuration:
+                if keysPressed[i] > slowestKey:
+                    slowestKey = keysPressed[i]
+            startTime = releaseTimes[i]
+        return slowestKey
