99760f60a5deceada8ee697d893964b4d1c6131f
weinlincheng <7236196+weilincheng@users.noreply.github.com>
2021-10-05 11:19:50 +0800

    Add new approach

--- dynamicProgramming/70_climbing_stairs.py
+++ dynamicProgramming/70_climbing_stairs.py
@@ -11,3 +11,24 @@ class Solution:
             two = temp
             
         return one
+
+    # Use an array to store the ways to reach certain stairs 
+    # Start from the beginning of the stairs 
+    # O(n) time | O(n) space - where n is the number of steps
+    def climbStairs2(self, n: int) -> int:
+        ways = [None for _ in range(n + 1)]
+        ways[0], ways[1] = 1, 1
+        for i in range(2, n + 1):
+            ways[i] = ways[i - 1] + ways[i - 2]
+        
+        return ways[n]
+
+    # Start from the beginning of the stairs 
+    # O(n) time | O(1) space - where n is the number of steps
+    def climbStairs3(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(2, n + 1):
+            temp = two
+            two = one + two
+            one = temp 
+        return two
