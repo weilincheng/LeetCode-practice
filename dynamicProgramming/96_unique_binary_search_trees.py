8bf95e3a978e288fca0c6f9c6c383a49c7faf029
weinlincheng <7236196+weilincheng@users.noreply.github.com>
2021-11-08 14:52:47 +0800

    Add 96. Unique Binary Search Trees

---
+++ dynamicProgramming/96_unique_binary_search_trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    # O(n^2) time | O(n) space - where n is the input integer
+    def numTrees(self, n: int) -> int:
+        numTree = [1] * (n + 1)
+        for nodes in range(2, n + 1):
+            total = 0
+            for root in range(1, nodes + 1):
+                left = root - 1
+                right = nodes - root
+                total += numTree[left] * numTree[right]
+            numTree[nodes] = total
+        return numTree[n]
+    
