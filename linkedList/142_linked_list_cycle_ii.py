303e4ef17c544e163e7b9ec1baaa4d8709c27779
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-01-19 10:47:29 +0800

    Add 142. Linked List Cycle II

---
+++ linkedList/142_linked_list_cycle_ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        try:
+            slow = head.next
+            fast = head.next.next
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next.next
+                
+        except:
+            return None
+        
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        return slow
+    
