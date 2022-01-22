5f2c6b1a6d29d40ebdc6632cc1618e9cadd955d2
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-01-22 11:15:07 +0800

    Add 1510. Stone Game IV

---
+++ dynamicProgramming/1510_stone_game_iv.py
@@ -0,0 +1,13 @@
+class Solution:
+    # O(n*sqrt(n)) time | O(n) space
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                if dp[i - j * j] == False:
+                    dp[i] = True
+                    break
+                j += 1
+        return dp[n]
+    
