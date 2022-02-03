da75a676c593b6683e9ef2a39cb9b4f7a9337097
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-02-03 12:22:05 +0800

    Add 454. 4Sum II

---
+++ hashTable/454_4sum_II.py
@@ -0,0 +1,16 @@
+class Solution:
+    # O(n^2) time | O(n) space 
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        seenNum = {}
+        for num1, num2 in product(nums1, nums2):
+            currSum = num1 + num2
+            seenNum[currSum] = seenNum.get(currSum, 0) + 1
+        
+        result = 0
+        for num3, num4 in product(nums3, nums4):
+            currSum = num3 + num4
+            if -currSum in seenNum:
+                result += seenNum[-currSum]
+        
+        return result 
+    
