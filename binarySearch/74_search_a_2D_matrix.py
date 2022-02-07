a8b682564538bc4589bc49d8809c1907b2e7b9f8
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-02-07 15:54:56 +0800

    Add 74. Search a 2D Matrix

---
+++ binarySearch/74_search_a_2D_matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    # O(log(m) + log(n)) time | O(1) space 
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        
+        topRow, bottomRow = 0, rows - 1
+        while topRow <= bottomRow:
+            midRow = topRow + (bottomRow - topRow) // 2
+            if matrix[midRow][0] <= target <= matrix[midRow][cols - 1]:
+                left, right = 0, cols - 1
+                while left <= right:
+                    mid = left + (right - left) // 2
+                    if target == matrix[midRow][mid]:
+                        return True
+                    elif target < matrix[midRow][mid]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                return False
+            elif target < matrix[midRow][0]:
+                bottomRow = midRow - 1
+            else:
+                topRow = midRow + 1
+        return False
+        
