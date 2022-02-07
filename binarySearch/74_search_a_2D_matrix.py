342359856871c935b724c40df698b8cf4b641cc3
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-02-07 16:10:02 +0800

    Update 74

--- binarySearch/74_search_a_2D_matrix.py
+++ binarySearch/74_search_a_2D_matrix.py
@@ -22,4 +22,20 @@ class Solution:
             else:
                 topRow = midRow + 1
         return False
-        
+
+class Solution2:
+    # O(log(m*n)) time | O(1) space
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        left, right = 0, rows * cols - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            r, c = mid // cols, mid % cols
+            midVal = matrix[r][c]
+            if midVal < target:
+                left = mid + 1
+            elif midVal > target:
+                right = mid - 1
+            else:
+                return True
+        return False
