82ff3624444cca31bbe844efaf052dd5340afbc2
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-02-21 16:48:14 +0800

    Add O(1) space solution

--- hashTable/169_majority_element.py
+++ hashTable/169_majority_element.py
@@ -7,4 +7,17 @@ class Solution:
             count[num] = count.get(num, 0) + 1
             if count[num] >= majoritySize:
                 return num
+
+class Solution2:
+    # O(n) time | O(1) space - where n is the lenght of the array
+    def majorityElement(self, nums: List[int]) -> int:
+        count, result = 0, 0
         
+        for num in nums:
+            if count == 0:
+                result = num
+            
+            count += 1 if result == num else -1
+            
+        return result
+         
