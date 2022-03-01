89b4ec75568818bcc905bb6ad3b781284f4e7a57
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-03-02 00:29:20 +0800

    Add solution

--- binarySearchTree/108_convert_sorted_array_to_binary_search_tree.py
+++ binarySearchTree/108_convert_sorted_array_to_binary_search_tree.py
@@ -1,5 +1,5 @@
 class Solution:
-    # O(n) time | O(n) space - where n is the length of the array 
+    # O(n) time | O(log(n)) space - where n is the length of the array 
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         def constructBST(bst, startIdx, endIdx):
             if startIdx > endIdx: 
@@ -23,3 +23,20 @@ class Solution:
             return bst
         
         return constructBST(None, 0, len(nums) - 1)
+
+class Solution2:
+    # O(n) time | O(log(n)) space - where n is the length of the input array
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def helper(startIdx, endIdx):
+            if startIdx > endIdx:
+                return None
+            
+            midIdx = startIdx + (endIdx - startIdx) // 2
+            root = TreeNode(nums[midIdx])
+            root.left = helper(startIdx, midIdx - 1)
+            root.right = helper(midIdx + 1, endIdx)
+            return root
+        
+        return helper(0, len(nums) - 1)
+        
