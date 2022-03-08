1a1649182c2af95a63c258f23c6d5e90874d73b2
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-03-08 11:32:01 +0800

    Update complexity analysis

--- linkedList/19_remove_nth_node_from_end_of_list.py
+++ linkedList/19_remove_nth_node_from_end_of_list.py
@@ -1,5 +1,5 @@
 class Solution:
-    # O(1) time | O(1) space
+    # O(n) time | O(1) space
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         first, second = head, head
         for _ in range(n):
@@ -15,3 +15,18 @@ class Solution:
         first.next = first.next.next
         
         return head
+
+class Solution2:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left, right = dummy, head
+        for _ in range(n):
+            right = right.next
+            
+        while right:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+        
+        return dummy.next
