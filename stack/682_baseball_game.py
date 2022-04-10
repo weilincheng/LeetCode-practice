af0d3245562662572c8f77f58d2c904a98d66f16
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-10 11:18:53 +0800

    Add 682. Baseball Game

---
+++ stack/682_baseball_game.py
@@ -0,0 +1,16 @@
+class Solution:
+    # O(n) time | O(n) space - where n is the length of the input array
+    def calPoints(self, ops: List[str]) -> int:
+        stack = []
+        for op in ops:
+            if op == "C":
+                stack.pop()
+            elif op == "D":
+                stack.append(stack[-1] *2)
+            elif op == "+":
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(op))
+        
+        return sum(stack)
+    
