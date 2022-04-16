50b8de456e7c95b5dc7f0981ff2eba31e7123d03
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-16 17:32:41 +0800

    Add 608. Tree Node

---
+++ SQL/608_tree_node.sql
@@ -0,0 +1,21 @@
+SELECT
+    id, 'Root' AS type
+FROM
+    Tree
+WHERE
+    p_id IS NULL
+UNION
+SELECT
+    id, 'Inner' AS type
+FROM 
+    Tree
+WHERE
+    id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
+UNION
+SELECT
+    id, 'Leaf' AS type
+FROM 
+    Tree
+WHERE
+    id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
+;
