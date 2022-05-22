f80bcbcb65b7387f11ca76c47a302f7e881361e7
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-05-22 10:49:20 +0800

    Update 647

--- dynamicProgramming/647_palindromic_substrings.py
+++ dynamicProgramming/647_palindromic_substrings.py
@@ -1,19 +1,16 @@
 class Solution:
     # O(n^2) time | O(1) space 
     def countSubstrings(self, s: str) -> int:
-        def checkLeftRight(l, r):
-            ans = 0
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                ans += 1
-                l, r = l - 1, r + 1
-            return ans
-        
         res = 0
         for i in range(len(s)):
-            l = r = i
-            res += checkLeftRight(l, r)
-            
-            l, r = i, i + 1
-            res += checkLeftRight(l, r)
+            res += self.countPalindrom(s, i, i)
+            res += self.countPalindrom(s, i, i + 1)
         
         return res
+
+    def countPalindrom(self, s, l, r):
+        ans = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            ans += 1
+            l, r = l - 1, r + 1
+        return ans
