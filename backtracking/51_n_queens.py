668dc10d162315e6f78d04ba0fc9b9b4384610a7
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-04 12:16:51 +0800

    Add 51. N-Queens

---
+++ backtracking/51_n_queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set() # r + c
+        negDiag = set() # r - c
+        res = []
+        board = [["." for _ in range(n)] for _ in range(n)]
+        
+        def backtrack(r):
+            if r == n: 
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return 
+        
+            for c in range(n):
+                if c in col or r + c in posDiag or r - c in negDiag:
+                    continue
+                board[r][c] = "Q"
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                
+                backtrack(r + 1)
+                
+                board[r][c] = "."
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+
+        backtrack(0)
+        return res
+
