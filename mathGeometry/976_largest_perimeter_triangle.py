0d547645cd9de48f2d5c9d56bff4aff1ae010a7b
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-10-12 09:18:23 +0800

    Add 976. Largest Perimeter Triangle

---
+++ mathGeometry/976_largest_perimeter_triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    # O(nlogn) time | O(n) space - where n is the length of the input array
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 3, -1, -1):
+            if nums[i] + nums[i + 1] > nums[i + 2]:
+                return nums[i] + nums[i + 1] + nums[i + 2]
+        
+        return 0
+
