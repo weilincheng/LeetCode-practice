db9f4343df5c842d0aada548256d320c3464d18e
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-12-17 17:23:21 +0800

    Add 232. Implement Queue using Stacks

---
+++ stack/232_implement_queue_using_stacks.go
@@ -0,0 +1,40 @@
+type MyQueue struct {
+	stack1 []int
+	stack2 []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{
+		stack1: []int{},
+		stack2: []int{},
+	}
+}
+
+func (this *MyQueue) Push(x int)  {
+    this.stack1 = append(this.stack1, x)    
+}
+
+
+func (this *MyQueue) Pop() int {
+    this.Peek()
+    res := this.stack2[len(this.stack2) - 1]
+	this.stack2 = this.stack2[:len(this.stack2) - 1]
+    return res
+}
+
+
+func (this *MyQueue) Peek() int {
+    if len(this.stack2) == 0 {
+        for i := len(this.stack1) - 1; i >= 0; i-- {
+            this.stack2 = append(this.stack2, this.stack1[i])
+        } 
+        this.stack1 = []int{}
+    }
+    return this.stack2[len(this.stack2) - 1]
+}
+
+
+func (this *MyQueue) Empty() bool {
+    return len(this.stack1) == 0 && len(this.stack2) == 0
+}
+
